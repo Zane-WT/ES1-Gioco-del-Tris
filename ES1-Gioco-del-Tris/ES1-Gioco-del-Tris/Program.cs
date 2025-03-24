ad7a6e8d6c57503d23d30f9c88faed37af822692
Luca Zanetti <zanetti.luca.22@itisriva.edu.it>
2025-03-24 14:40:41 +0100

    Implementazione della Matrice

---
+++ ES1-Gioco-del-Tris/ES1-Gioco-del-Tris/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ES1_Gioco_del_Tris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] table = new int[3, 3];
+        }
+    }
+}
