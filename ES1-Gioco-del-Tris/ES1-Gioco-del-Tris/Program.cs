e0d08b7f2f5a91ecf9e1efdf9aec523e0387e99f
Luca Zanetti <zanetti.luca.22@itisriva.edu.it>
2025-03-24 15:48:16 +0100

    Implementazione dei turni dei 2 player

--- ES1-Gioco-del-Tris/ES1-Gioco-del-Tris/Program.cs
+++ ES1-Gioco-del-Tris/ES1-Gioco-del-Tris/Program.cs
@@ -4,33 +4,77 @@
     {
         static void Main(string[] args)
         {
-            int[,] table = new int[3, 3];
+            char[,] table = new char[3, 3];
 
             Console.WriteLine("Inserire nome del giocatore 1");
             string player_1 = Console.ReadLine();
             Console.WriteLine("Inserire nome del giocatore 2");
             string player_2 = Console.ReadLine();
             bool risultatoFinale = false;
+            Random rand = new Random();
+            int turnoGiocatore = rand.Next(1, 3);
+            
             do 
             {
-                stampaGrafica(table);
-            }while (false);
+                    for (int i = 0; i < 1; i++) 
+                { 
+                    if (turnoGiocatore == 1)
+                    {
+                         TurnoPlayer_1(table,player_1);
+                        turnoGiocatore++;
+                    }
+                    else
+                    {
+                         TurnoPlayer_2(table, player_2);
+                        turnoGiocatore--;
+                    
+                    }
+                }       
+            } while (true);
             
 
 
         }
-        static void stampaGrafica(int[,] box)
+        static void stampaGrafica(char[,] box)
         {
             for(int i = 0; i < 3; i++) 
             {
-                Console.WriteLine($"");      
+                Console.WriteLine($"");
+                Console.Write(i + 1);
                 for(int j = 0; j < 3; j++)
                 {
-                    Console.Write($"| {box[i,j]} |");  
+                    
+
+                    Console.Write($" |\t{box[i,j]}\t|");  
                 }
                
                 
             }
         }
-    }
+        static void TurnoPlayer_1(char[,] box, string nome)
+        {
+            Console.WriteLine($"\nTurno di {nome} il tuo segno è lo 'O' ");
+            Console.WriteLine("In che posizione metti il tuo segno:");
+            Console.Write("Righa:");
+            int righa_player_1 = int.Parse(Console.ReadLine());
+            Console.Write("Colonna:");
+            int colonna_player_1 = int.Parse(Console.ReadLine());
+            box[righa_player_1 - 1, colonna_player_1 - 1] = 'O';
+            stampaGrafica(box);
+            
+        }
+        static void TurnoPlayer_2(char[,] box,  string nome)
+        {
+            Console.WriteLine($"\nTurno di {nome} il tuo segno è lo 'X' ");
+            Console.WriteLine("In che posizione metti il tuo segno:");
+            Console.Write("Righa:");
+            int righa_player_1 = int.Parse(Console.ReadLine());
+            Console.Write("Colonna:");
+            int colonna_player_1 = int.Parse(Console.ReadLine());
+            box[righa_player_1 - 1, colonna_player_1 - 1] = 'X';
+            stampaGrafica(box);
+            
+        }
+
+    }  
 }
