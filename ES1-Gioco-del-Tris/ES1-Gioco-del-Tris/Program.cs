a8471b1e08906c5539c9a1fdf7a5c0a6012e2b70
Luca Zanetti <zanetti.luca.22@itisriva.edu.it>
2025-03-24 15:06:19 +0100

    Creazione Funzione stampa matrice

--- ES1-Gioco-del-Tris/ES1-Gioco-del-Tris/Program.cs
+++ ES1-Gioco-del-Tris/ES1-Gioco-del-Tris/Program.cs
@@ -10,8 +10,27 @@
             string player_1 = Console.ReadLine();
             Console.WriteLine("Inserire nome del giocatore 2");
             string player_2 = Console.ReadLine();
+            bool risultatoFinale = false;
+            do 
+            {
+                stampaGrafica(table);
+            }while (false);
+            
 
+
+        }
+        static void stampaGrafica(int[,] box)
+        {
+            for(int i = 0; i < 3; i++) 
+            {
+                Console.WriteLine($"");      
+                for(int j = 0; j < 3; j++)
+                {
+                    Console.Write($"| {box[i,j]} |");  
+                }
+               
                 
+            }
         }
     }
 }
